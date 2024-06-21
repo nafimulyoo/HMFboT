4f36f9a48a5edb3cb1dcd212bed96b77cebbfd7f
nafimulyoo <youremail@domain.com>
2024-06-22 03:57:48 +0700

    simplify

--- command.js
+++ command.js
@@ -130,7 +130,7 @@ const parseCommand = async (command) => {
         }
 
        
-        if (lowerCommand.startsWith('/broadcast ')) {
+        if (lowerCommand.startsWith('/broadcast ') || lowerCommand.startsWith('/broadcast\n')) {
             let message = command.replace('/broadcast ', '').trim();
             if (!message) {
                 return {
