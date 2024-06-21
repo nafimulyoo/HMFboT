31a92bb4561dd6324fa66efd4f21e51d2ba024b8
nafimulyoo <youremail@domain.com>
2024-06-22 01:33:49 +0700

    test userid

--- index.js
+++ index.js
@@ -55,10 +55,10 @@ async function handleEvent(event) {
   if (event.message.text == '/getuserid') {
     return client.replyMessage({
       replyToken: event.replyToken,
-      messages: {
+      messages: [{
         type: 'text',
         text: `Your user id is ${event.source.userId}`
-      }
+      }]
     });
   }
   
